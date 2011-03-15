e9ec735f2bd6cda8b8fa81cf2954215b1611165e
Lars Christensen <larsch@belunktum.dk>
2011-03-15 14:09:49 +0100

    Clean also downloads

--- bin/cleantemp.rb
+++ bin/cleantemp.rb
@@ -29,27 +29,31 @@ end
 # 2 days
 MAX_AGE = 2*24*60*60
 
-total_size = 0
-file_count = 0
-dir_count = 0
-
-files(ENV['TEMP']) do |path|
-  age = Time.now - File.mtime(path)
-  if age > MAX_AGE
-    total_size += File.size(path)
-    file_count += 1
-    File.unlink(path)
-  end
-end
-
-directories(ENV['TEMP']) do |path|
-  if Dir.entries(path).size == 2
-    begin
-      Dir.rmdir(path)
-      dir_count += 1
-    rescue Errno::EACCES
+def clean(clean_path, max_age = MAX_AGE)
+  total_size = 0
+  file_count = 0
+  dir_count = 0
+  files(clean_path) do |path|
+    age = Time.now - File.mtime(path)
+    if age > MAX_AGE
+      total_size += File.size(path)
+      file_count += 1
+      File.unlink(path) rescue nil
     end
   end
+  directories(clean_path) do |path|
+    if Dir.entries(path).size == 2
+      begin
+        Dir.rmdir(path)
+        dir_count += 1
+      rescue Errno::EACCES
+      end
+    end
+  end
+  puts "Removed #{file_count} files (#{total_size} bytes) and #{dir_count} directories from #{clean_path}."
 end
-
-puts "Removed #{file_count} files (#{total_size} bytes) and #{dir_count} directories."
+clean(ENV['TEMP'])
+clean(ENV['USERPROFILE'] + '\My Documents\Downloads', 31*24*60*60)
+system "net stop wuauserv"
+clean(ENV['SystemRoot'] + '\SoftwareDistribution\Download')
+system "net start wuauserv"
