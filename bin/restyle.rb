46cb822b29118de75fd9fe362ec1075adf456a28
Lars Christensen <larsch@belunktum.dk>
2012-04-29 14:57:30 +0200

    customize?

--- bin/restyle.rb
+++ bin/restyle.rb
@@ -8,24 +8,112 @@
 FILENAME_PATTERN = /\.(h|c|cpp|hpp|cxx|hxx|c++|h++)$/
 ENCODING = 'ISO-8859-1'
 ASTYLE_EXECUTABLE = "astyle"
-ASTYLE_OPTIONS =  ["-q", "--style=allman", "-Os3YHUjck1M60EK" ]
+ASTYLE_OPTIONS =  ["-q"]
 
-@skip_file_if_handlers = []
+ASTYLE_RE = /x?[a-zA-Z]\d*/
+ASTYLE_FLAGS = "A1Os3YHUjck1M60EK".scan(ASTYLE_RE)
+
+DEFAULT_OPTIONS = {
+  "astyle" => "-A1Os3YHUjck1M60EK",
+  "restyle" => "",
+}
+
+def replace_or_add(a, opt)
+  opt =~ /^x?[a-zA-Z]/i
+  optch = $&
+  if old_opt = a.find { |o| o.index(optch) == 0 }
+    old_opt.replace(opt)
+  else
+    a.push(opt)
+  end
+end
+
+def replace_or_add_all(a, opts)
+  opts.each { |opt| replace_or_add(a, opt) }
+end
+
+def remove_all(a, opts)
+  opts.each do |opt|
+    opt =~ /^x?[a-zA-Z]/i
+    optch = $&
+    a.delete_if { |o| o.index(optch) == 0 }
+  end
+end
+
+def merge_style_flags(a, b)
+  aset = a.scan(ASTYLE_RE)
+  b.strip.split(/\s+/).each do |bopt|
+    case bopt[0]
+    when "-"
+      replace_or_add_all(aset, bopt[1..-1].scan(ASTYLE_RE))
+    when "+"
+      remove_all(aset, bopt[1..-1].scan(ASTYLE_RE))
+    else
+      raise "Unrecognized option: #{bopt}"
+    end
+  end
+  "-" + aset.join
+end
+
+def merge_options(a, b)
+  opts = a.dup
+  b.each do |key, val|
+    case key
+    when "astyle", "restyle"
+      opts[key] = merge_style_flags(opts[key], val)
+    else
+      opts[key] = val
+    end
+  end
+  opts
+end
+
+def get_content_options(content)
+  opts = {}
+  opt_re = /(astyle|restyle):\s*(.*)/
+  comment_re = /(?:\/\/\s*#{opt_re}\s*$|\/\*\s*#{opt_re}\s*\*\/)/
+  content.scan(opt_re) { |opt1,arg1,opt2,arg2| opts[opt1 || opt2] = arg1 || arg2 }
+  opts
+end
+
+def load_dot_restyle(file)
+  require 'yaml'
+  YAML.parse_file(file).transform
+end
+
+def get_style_options(file)
+  files = []
+  path = File.expand_path(file)
+  while ((superpath = File.dirname(path)) != path)
+    if File.exist?(dot_restyle = File.join(superpath, ".restyle"))
+      files << dot_restyle
+    end
+    path = superpath
+  end
+  opts = DEFAULT_OPTIONS
+  files.reverse_each do |dot_restyle|
+    opts = merge_options(opts, load_dot_restyle(dot_restyle))
+  end
+  return opts
+end
 
 # Restyle the contents of a file.
-def restyle(file)
+def restyle_file(file)
+  opts = get_style_options(file)
   content = File.read(file, :encoding => ENCODING)
-  if not $opts.force and skip_file(content)
+  if not $opts.force and opts["content_guard"] and not content.match(opts["content_guard"])
     log "Skipping #{file}"
     return
   end
+  opts = merge_options(opts, get_content_options(content))
+  
   orig_content = content.dup
-  restyle_code(content)
+  restyle(content)
   restyle_headerguards(content, file) if file =~ /\.(h|hpp|hxx)$/
 
   temp_file = File.join(ENV["TEMP"], "astyletemp")
   File.open(temp_file, "w") { |f| f << content }
-  options = ASTYLE_OPTIONS + [temp_file]
+  options = ASTYLE_OPTIONS + [opts["astyle"], temp_file]
   system(ASTYLE_EXECUTABLE, *options)
   content = File.read(temp_file, :encoding => ENCODING)
 
@@ -43,8 +131,10 @@ def restyle(file)
   end
 end
 
+EMPTY_LINE = /^(?:\s*)\n/
+
 # Restyle a chunk of code (our own rule set)
-def restyle_code(code)
+def restyle(code)
   # Adjust copyright year
   year = Time.now.year
   code.gsub!(/Copyright \S+ ((\d+)(-\d+)?)/) { |m| "Copyright © #{year}" }
@@ -52,10 +142,10 @@ def restyle_code(code)
   code.gsub!(/(\S)\s*\z/m, "\\1\n")
   # Only 1 empty line between sections
   code.gsub!(/(\n[ \t]*\n)\s*\n/m, "\\1")
-  # No empty lines after open-brace
-  code.gsub!(/\{\s*\n/m, "{\n")
-  # No empty lines before close-brace
-  code.gsub!(/\s*(\n *\})/, "\\1")
+  # No empty lines after open-brace (if something is indented after)
+  code.gsub!(/\{\s*^(\s+\S)\n/m, "{\n\\1")
+  # No empty lines before close-brace (if something was intended before)
+  code.gsub!(/^( +\S.*?\n)\s*(\n *\})/, "\\1\\2")
   # Centre lines in file header
   code.sub!(/\A\s*\/\/={54}\n(\/\/(.*\n))+?\/\/-{54}/) { |x|
     x.split("\n").map { |ln|
@@ -66,11 +156,14 @@ def restyle_code(code)
   }
 
   # /* comment */ to // comment
-  code.gsub!(/\/\*(.*?)\*\//) { "// " + $1.strip }
+  code.gsub!(/\/\*(.*?)\*\/ *$/) { "// " + $1.strip }
   # Space after //
-  code.gsub!(/\/\/(\S)/, "// \\1")
+  code.gsub!(/\/\/([a-z0-9])/i, "// \\1")
   # Forward slashes in includes
   code.gsub!(/^(\s*#\s*include\s+["<])(.*?)([>"])/) { $1 + $2.tr('\\','/') + $3 }
+  # At least one empty line before comments
+  # code.gsub!(/#{EMPTY_LINE}*^(\s*\/(?:\/|\*))/, "\n\\1")
+  code
 end
 
 def determine_headerguard(path)
@@ -191,7 +284,7 @@ if __FILE__ == $0
       next
     end
     if path =~ FILENAME_PATTERN
-      restyle(path)
+      restyle_file(path)
     end
   end
   log "#{ARGV.size} file(s) processed"
