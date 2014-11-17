a4ee2ec5f1ac98ce736f7cc2393eac55d0e60939
Lars Christensen <larsch@belunktum.dk>
2014-11-17 12:16:50 +0100

    Better passwords

--- bin/pwgen.rb
+++ bin/pwgen.rb
@@ -1,3 +1,9 @@
 #!/usr/bin/env ruby
-chars = ("a".."z").to_a.join + ("A".."Z").to_a.join + "0..9" + "!/?_$%&*+-,.;:"
-puts Array.new(10) { chars[rand(chars.length)] }.join
+LETTERS = (?a..?z).to_a - [?l]
+UPLETTERS = (?A..?Z).to_a - [?O]
+DIGITS = (?0..?9).to_a - [?0, ?1]
+CONTROL = "!/?_$%&*+-,.;:"
+ALL = [LETTERS, UPLETTERS, DIGITS, CONTROL].join
+def rnd(rng); rng[rand(rng.size)]; end
+LENGTH = 10
+puts ([rnd(LETTERS), rnd(UPLETTERS), rnd(DIGITS), rnd(CONTROL)] + Array.new(LENGTH - 4) { rnd(ALL) }).shuffle.join
