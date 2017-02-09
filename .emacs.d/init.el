70a19fd2c7f293b09d2b1b2c8a6a4f9aa05004b8
Lars Christensen <larsch@belunktum.dk>
2017-02-09 19:19:05 +0100

    Syntax fix

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -666,7 +666,7 @@ non-nil."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
  '(package-selected-packages
    (quote
-    (scad-mode dokuwiki-mode markdown-mode hungry-delete whitespace-cleanup-mode visual-regexp-steroids powerline pcre2el json-mode jade-mode))))
+    (scad-mode dokuwiki-mode markdown-mode hungry-delete whitespace-cleanup-mode visual-regexp-steroids powerline pcre2el json-mode jade-mode)))
+ )
