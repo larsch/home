7eca4063b214074c8bba3761ecc52bafe5382fbe
Lars Christensen <larsch@belunktum.dk>
2018-02-08 17:15:04 +0100

    Keep customizations elsewhere

---
+++ .emacs.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yasnippet yaml-mode whitespace-cleanup-mode visual-regexp-steroids rust-mode rg powerline pcre2el markdown-mode json-mode js2-mode jade-mode iedit hungry-delete htmlize editorconfig-custom-majormode ag))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,17 +1,3 @@
 (package-initialize)
 (require 'org)
 (org-babel-load-file (expand-file-name "settings.org" user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (yasnippet yaml-mode whitespace-cleanup-mode visual-regexp-steroids rust-mode rg powerline pcre2el markdown-mode json-mode js2-mode jade-mode iedit hungry-delete htmlize editorconfig-custom-majormode ag))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
