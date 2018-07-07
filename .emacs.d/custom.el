a1297f09c35f72519d9a15846096ac687553a94a
Lars Christensen <larsch@belunktum.dk>
2018-07-07 08:04:09 +0200

    custom.el: update

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("be29e7507638de0e6b458112033d05dff55b224aa8cbffa71eeb1596ea4aabf9" default)))
+    ("a81bc918eceaee124247648fc9682caddd713897d7fd1398856a5b61a592cb62" "be29e7507638de0e6b458112033d05dff55b224aa8cbffa71eeb1596ea4aabf9" default)))
  '(package-selected-packages
    (quote
     (expand-region nlinum dockerfile-mode flycheck-rust org-plus-contrib org-edna org iedit visual-regexp editorconfig yasnippet yaml-mode whitespace-cleanup-mode visual-regexp-steroids rust-mode rg powerline pcre2el markdown-mode json-mode js2-mode jade-mode hungry-delete htmlize editorconfig-custom-majormode ag))))
