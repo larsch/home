f7c85aeb57fbce22083ebd8e25f678c809ea9177
Lars Christensen <larsch@belunktum.dk>
2019-07-17 22:52:48 +0200

    .emacs/custom.el: remove custom-set-faces

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -8,10 +8,4 @@
     ("a81bc918eceaee124247648fc9682caddd713897d7fd1398856a5b61a592cb62" "be29e7507638de0e6b458112033d05dff55b224aa8cbffa71eeb1596ea4aabf9" default)))
  '(package-selected-packages
    (quote
-    (expand-region nlinum dockerfile-mode flycheck-rust org-plus-contrib org-edna org iedit visual-regexp editorconfig yasnippet yaml-mode whitespace-cleanup-mode visual-regexp-steroids rust-mode rg powerline pcre2el markdown-mode json-mode js2-mode jade-mode hungry-delete htmlize editorconfig-custom-majormode ag))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+    (dockerfile-mode org-plus-contrib org-edna org iedit visual-regexp editorconfig yasnippet yaml-mode whitespace-cleanup-mode visual-regexp-steroids rust-mode rg powerline pcre2el markdown-mode json-mode js2-mode jade-mode hungry-delete htmlize editorconfig-custom-majormode ag))))
